fd94d2b7f9dc98ec5c3779cb1cff4428ec3d1346
Jakob Thorsbro <s134265@student.dtu.dk>
2019-01-08 14:21:19 +0100

    Fixed dist.h

--- tune_labs/distance.c
+++ tune_labs/distance.c
@@ -7,7 +7,11 @@ distance(particle_t *p, int n) {
     int i;
     double dist = 0;
     for(i=0;i<n;i++){
-        p[i].dist=pow(pow(p[i].x,2)+pow(p[i].y,2)+pow(p[i].z,2),0.5);
+        p[i].dist=p[i].x*p[i].x+p[i].y*p[i].z+p[i].y*p[i].z;
+        
+    }
+    for(i=0;i<n;i++){
+        p[i].dist= sqrt(p[i].dist);
         dist += p[i].dist;
     }
     return dist;
@@ -18,8 +22,12 @@ distance(particle_t p, int n) {
     int i;
     double dist = 0;
     for(i=0;i<n;i++){
-        p.dist[i]=pow(pow(p.x[i],2)+pow(p.y[i],2)+pow(p.z[i],2),0.5);
-        dist+=p.dist[i];
+        p.dist[i]=p.x[i]*p.x[i]+p.y[i]*p.y[i]+p.z[i]*p.z[i];
+    }
+    }
+    for(i=0;i<n;i++){
+        p[i].dist= sqrt(p.dist[i]);
+        dist += p.dist[i];
     }
     return dist;
 }
