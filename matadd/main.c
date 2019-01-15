e5e885a8f485537de95793367a360bfa45d70a27
s134265 <s134265@student.dtu.dk>
2019-01-15 10:45:37 +0100

    exercies

--- matadd/main.c
+++ matadd/main.c
@@ -13,10 +13,10 @@
 int
 main(int argc, char *argv[]) {
 
-    int    i,j,l, m=3, n=2, k=5, N = NREPEAT;
+    int    i,j,l, m=30000, n=20000, k=50000, N = NREPEAT;
     double **A, **B, **C, **D;
     double alf=1.0,bet=0.0;
-    //double *b,*c;
+    double *b,*c;
     double tcpu1,tcpu2; 
 
     clock_t t1, t2, t3, t4;
@@ -25,53 +25,52 @@ main(int argc, char *argv[]) {
 	//n = m + 25;
 
 	/* Allocate memory */
-	A = malloc_2d(m, k);
+	A = malloc_2d(m, n);
 	B = malloc_2d(k, n);
-	C = malloc_2d(m, n);
-	D = malloc_2d(m, n);
-	//b = malloc(n * sizeof(double));
-	//c = malloc(n * sizeof(double));
-	if (A == NULL || B == NULL | C == NULL ){ //| b == NULL | c == NULL) {
+	//C = malloc_2d(m, n);
+	//D = malloc_2d(m, n);
+	b = malloc(n * sizeof(double));
+	c = malloc(n * sizeof(double));
+	if (A == NULL){// || B == NULL | C == NULL ){ //| b == NULL | c == NULL) {
 	    fprintf(stderr, "Memory allocation error...\n");
 	    exit(EXIT_FAILURE);
 	}
 	
-	//init_1d(n,b);
+	init_1d(n,b);
 	/* initialize with useful data - last argument is reference */
-	//init_data(m,n,A,B);
-	init_data_mult(m,n,k,A,B);
+	init_data(m,n,A,B);
+	//init_data_mult(m,n,k,A,B);
 	/* timings for matadd */
 	t1 = mytimer();
-	
 	//for (i = 0; i < N; i++)
-	matmult(m,n,k,A,B,C);
+	//matmult(m,n,k,A,B,C);
 	    //matadd(m, n, A, B, C);
-		//matmultvec(m,n,A,b,c);
+	matmultvec(m,n,A,b,c);
 	t2 = mytimer();
-	t3 = mytimer();
+	//t3 = mytimer();
 
-	cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,m,n,k,alf,*A,k,*B,n,bet,*D,n);
-	t4 = mytimer();
+	//cblas_dgemm(CblasRowMajor,CblasNoTrans,CblasNoTrans,m,n,k,alf,*A,k,*B,n,bet,*D,n);
+	//t4 = mytimer();
 	tcpu1 = delta_t(t1, t2);
-	tcpu2 = delta_t(t3, t4);
-	for (l = 0; l<m; l++)
-		for (j=0; j<n; j++)
-			printf("%8.3f\n", C[l][j]);
-	for (l = 0; l<m; l++)
-		for (j=0; j<n; j++)
-			printf("%8.3f\n", D[l][j]);
+	//tcpu2 = delta_t(t3, t4);
+	//for (l = 0; l<m; l++)
+		//for (j=0; j<n; j++)
+			//printf("%8.3f\n", C[l][j]);
+	//for (l = 0; l<m; l++)
+	//for (j=0; j<n; j++)
+		//printf("%8.3f\n", c[j]);
 	//check_results("main", m, n, C);
 
 	/* Print n and results  */
-	printf("%8.3f %8.3f\n", tcpu1, tcpu2);
+	printf("%8.3f\n", tcpu1);
 
 	/* Free memory */
 	free_2d(A);
-	free_2d(B);
-	free_2d(C);
-	free_2d(D);
-	//free(b);
-	//free(c);
+	//free_2d(B);
+	//free_2d(C);
+	//free_2d(D);
+	free(b);
+	free(c);
     //}
 
     return EXIT_SUCCESS;
