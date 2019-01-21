4cb4ed2229afa589cbaf2e661f694ea5cf7db2ea
Jakob Thorsbro <s134265@student.dtu.dk>
2019-01-21 12:05:43 +0100

    new ex

--- hello_world/helloworld.cu
+++ hello_world/helloworld.cu
@@ -1,12 +1,12 @@
 #include <stdio.h>
-
+#include <helper_cuda.h>
 
 __global__ void printgpu(){
   printf("Hello world! I’m thread %d out of 64 in block %d. My global thread id is %d out of 256.\n", threadIdx.x, blockIdx.x, blockIdx.x*blockDim.x+threadIdx.x);
 }
 
 
-int main()
+int main(int argc, char *argv[])
 {
   printgpu<<<4,64>>>();
 }
