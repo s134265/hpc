e51c62cbffa63c688c2592a2212321da07b81335
Jakob Thorsbro <s134265@student.dtu.dk>
2019-01-21 11:59:47 +0100

    new ex

--- hello_world/helloworld.cu
+++ hello_world/helloworld.cu
@@ -3,11 +3,10 @@
 
 __global__ void printgpu{
   printf("Hello world! I’m thread %d out of 64 in block %d. My global thread id is %d out of 256.\n", blockIdx.x, blockDim.x, blockIdx.x*blockDim.x)
-  return 0;
 }
 
 
 int main()
 {
-  printgpu<<<4,64>>>;
+  printgpu<<<4,64>>>();
 }
